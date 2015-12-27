fd202cefb500a0c7efc8f3fec4db8c9fcb4e6646
DarkPark <darkpark@pisem.net>
2015-12-27 13:23:49 +0200

    add gulp main entry point

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/**
+ * Gulp main entry point.
+ *
+ * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// use all default STB tasks
+require('stb-gulp');
