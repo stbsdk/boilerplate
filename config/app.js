1102846badd34023c0f5c75d2ec1af586aeb5b3f
DarkPark <darkpark@pisem.net>
2015-12-27 13:47:22 +0200

    add config files

---
+++ config/app.js
@@ -0,0 +1,12 @@
+/**
+ * Global application configuration.
+ * Can store run-time options, paths, flags and so on.
+ *
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// public
+module.exports = {};
