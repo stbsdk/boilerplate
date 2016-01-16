b88bd52da172bb938057b25e5cc7615a4f558874
Stanislav Kalashnik <sk@infomir.eu>
2016-01-16 14:28:41 +0200

    change init order

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@
 
 'use strict';
 
-// load STB gulp modules
-require('stb-gulp');
-
 // tasks configurations
 // to overwrite defaults
 module.exports = {};
+
+// load STB gulp modules
+require('stb-gulp');
