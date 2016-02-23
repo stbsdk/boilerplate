d396c392a1a95679f8601bc80e96ea82de0d5bfd
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-02-23 16:41:06 +0200

    replace addListeners with set of once

--- src/js/main.js
+++ src/js/main.js
@@ -8,20 +8,18 @@ var app    = require('stb-app'),
     router = require('spa-router');
 
 
-// main application events
-app.addListeners({
-    // all resources are loaded
-    load: function load () {
-        // set pages
-        router.init([
-            require('./pages/init'),
-            require('./pages/main')
-        ]);
-    },
+// all resources are loaded
+app.once('load', function () {
+    // set pages
+    router.init([
+        require('./pages/init'),
+        require('./pages/main')
+    ]);
+});
 
-    // everything is ready
-    done: function done () {
-        // go to the main page when necessary
-        router.navigate('pageMain');
-    }
+
+// everything is ready
+app.once('done', function () {
+    // go to the main page when necessary
+    router.navigate('pageMain');
 });
