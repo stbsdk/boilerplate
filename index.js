1f1498405c2efee8b77a08364528f8009a076221
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-08-12 16:57:53 +0300

    rework sdk init procedure

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Main SDK entry point.
+ */
+
+'use strict';
+
+// redefinition of plugin options example
+// require('stb-plugin-ssh/config').release.host = '192.168.1.20';
+
+// load default plugins
+// and run default tasks
+module.exports = require('stbsdk/default');
