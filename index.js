fca7d1e7d00d7566a53a91ebf20c5d56a7199019
Stanislav Kalashnik <darkpark.main@gmail.com>
2017-01-11 17:20:45 +0200

    add gettext plugin commented example

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
 // redefinition of plugin option example
 // require('stb-plugin-ssh/config').release.host = '192.168.1.20';
 
+// activate gettext languages
+// require('spa-plugin-gettext/config').default.languages = ['ru', 'fr'];
+
 // load default plugins
 // and run default tasks
 module.exports = require('stbsdk/default');
