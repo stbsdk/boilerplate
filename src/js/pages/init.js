7dc5a9d6e905c3f093be76a8c10a3b1acfc8e342
Stanislav Kalashnik <s.kalashnik@infomir.com>
2017-11-02 18:55:47 +0200

    use document.getElementById instead of window object

--- src/js/pages/init.js
+++ src/js/pages/init.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var Page = require('stb-component-page'),
-    page = new Page({$node: window.pageInit});
+    page = new Page({$node: document.getElementById('pageInit')});
 
 
 // public
--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var Page = require('stb-component-page'),
-    page = new Page({$node: window.pageMain});
+    page = new Page({$node: document.getElementById('pageMain')});
 
 
 // public
